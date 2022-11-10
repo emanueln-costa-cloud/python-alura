3b7da2e23755c825d821cc1828562eb2b529cdcf
emanueln-costa-cloud <emmanuelvitorhaimsp@gmail.com>
2022-11-10 15:45:31 -0300

    adicionando limitador condicional or com if e o continue

--- Curso1/adivinhacao.py
+++ Curso1/adivinhacao.py
@@ -7,9 +7,13 @@ total_de_tentativas = 3
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("Digite o seu numero: ")
+    chute_str = input("Digite um numero entre 1 e 100: ")
     print("Voce digitou", chute_str)
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Voce deve digitar um número entre 1 e 100!")
+        continue
 
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
