5aa8482768718c42970a3f485a1d942ce623b53b
emanueln-costa-cloud <emmanuelvitorhaimsp@gmail.com>
2022-11-12 12:25:03 -0300

    adicionando niveis de dificuldade

--- Curso1/adivinhacao.py
+++ Curso1/adivinhacao.py
@@ -5,8 +5,19 @@ print("Bem vindo no jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = random.randrange(1, 101)
-total_de_tentativas = 3
-print(numero_secreto)
+total_de_tentativas = 0
+
+print("Qual nivel de dificuldade? ")
+print("(1) Facil (2) Medio (3) Dificil")
+
+nivel = int(input("Define o nivel: "))
+
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
