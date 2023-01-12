f55314f95490aba027d1de34a233f7088a65b46a
emanueln-costa-cloud <emmanuelvitorhaimsp@gmail.com>
2023-01-12 18:38:51 -0300

    Implementando o for e if para encontrar as letras e a posição

--- Curso1/forca.py
+++ Curso1/forca.py
@@ -5,11 +5,21 @@ def jogar():
     print("*********************************")
     
     palavra_secreta = "banana"
+    
     enforcou = False
     acertou = False
 
     #enquanto nao enforcou(False)=True e nao acertou(False)=True
     while(not enforcou and not acertou):
+
+        chute = input("Qual letra?")
+        
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
         print("jogando...")
 
     print("Fim do jogo")
