92315754b1ecce086582fe7a1f4f269d9292ba6e
emanueln-costa-cloud <emmanuelvitorhaimsp@gmail.com>
2023-01-13 17:24:45 -0300

    Atualizando o codigo e implemantando contador de tentativas

--- Curso1/forca.py
+++ Curso1/forca.py
@@ -4,27 +4,34 @@ def jogar():
     print("Bem vindo no jogo da Forca!")
     print("*********************************")
     
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_acertadas = ["_","_","_","_","_","_"]
     
     enforcou = False
     acertou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     #enquanto nao enforcou(False)=True e nao acertou(False)=True
     while(not enforcou and not acertou):
 
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
         
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra 
-            index = index + 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra 
+                index += 1
+        else:
+            erros += 1
 
+        enforcou = erros == 6
         print(letras_acertadas)
 
-    print("Fim do jogo")
+    print("Enforcou, Fim do jogo")
 
 if(__name__ == "__main__"):
     jogar()
